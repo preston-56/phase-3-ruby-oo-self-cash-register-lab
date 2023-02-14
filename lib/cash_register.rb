4dc8ca7ce155841ccf3b93a7cda1c9d2f03e147d
Preston <prestonosoro56@gmail.com>
2023-02-14 19:01:33 +0300

    commit tree 8d5e49dc05bb98c94022eb2d3e10bd4795d1be79

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,31 @@
+class CashRegister
+    #macros accessor 
+    attr_accessor :total, :discount, :items, :last_transaction
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+        @items = [] 
+    end
 
+        #instance method add_item
+    def add_item(title,amount,quantity = 1)
+        self.last_transaction = amount * quantity
+        self.total += self.last_transaction
+        quantity.times { self.items << title}
+    end
+
+    #instant method apply discount 
+    def apply_discount
+        if self.discount != 0
+            self.total = (self.total * (100.0 - self.discount.to_f) / 100).to_i
+            "After the discount, the total comes to $#{self.total}."
+        else
+            "There is no discount to apply."
+        end
+    end
+
+    #instant method last_transaction
+    def void_last_transaction
+        self.total -= self.last_transaction
+    end
+end
