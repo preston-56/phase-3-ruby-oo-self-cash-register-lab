0449a5856df462ae791229284adbe8e1b473da23
Preston <prestonosoro56@gmail.com>
2023-02-14 19:06:05 +0300

    feat:define instance methods

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,31 @@
+class CashRegister
+    #macros accessor 
+    attr_accessor :total, :discount, :items, :last_transaction
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+        @items = [] 
+    end
 
+        #add_item instance method
+    def add_item(title,amount,quantity = 1)
+        self.last_transaction = amount * quantity
+        self.total += self.last_transaction
+        quantity.times { self.items << title}
+    end
+
+    #instant apply_discount method
+    def apply_discount
+        if self.discount != 0
+            self.total = (self.total * (100.0 - self.discount.to_f) / 100).to_i
+            "After the discount, the total comes to $#{self.total}."
+        else
+            "There is no discount to apply."
+        end
+    end
+
+    #last_transaction instant method
+    def void_last_transaction
+        self.total -= self.last_transaction
+    end
+end
